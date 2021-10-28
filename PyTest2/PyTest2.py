df78ddddfe4af26246f5398278ef35a73f41e63f
Гавриил Морозов <jerryfrost28@yandex.ru>
2021-10-28 18:31:17 +0300

    commit 4

--- PyTest2/PyTest2.py
+++ PyTest2/PyTest2.py
@@ -1,4 +1,31 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-print("Hello, World!")
+print("Hello, World!\n")
+
+def strToFloat(x):
+    y = ""
+    for i in x:
+        if (i != ','):
+            y += i
+        else:
+            y += '.'
+    return float(y)
+
+r = requests.get("https://yandex.ru/")
+
+html = bs(r.content, 'html.parser')
+
+convert = html.find_all("span", {"class" : "inline-stocks__value_inner"})
+
+price = {"USD" : strToFloat(convert[0].text) , 
+         "EUR" : strToFloat(convert[1].text) , 
+         "OIL" : strToFloat(convert[2].text)
+         }
+
+for k, v in price.items():
+    print(k, ':', v)
+
+print("\nBye, World!\n")
+
+print('evfefvevefve')
